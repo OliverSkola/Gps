a660e27fa2018d89d58a207570301e47ddb7c05f
chrmeis <chrmeis@student.chalmers.se>
2022-11-10 09:07:56 +0100

    zoomar in på set location i MapDisplayer

--- app/src/main/java/com/example/gps/MapFragment.java
+++ app/src/main/java/com/example/gps/MapFragment.java
@@ -1,24 +1,31 @@
 package com.example.gps;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 
 public class MapFragment extends Fragment {
 
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +41,18 @@ public class MapFragment extends Fragment {
         suppportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+                LatLng latLng=new LatLng(57.7089, 11.9746);
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        latLng, 13));
+
+                googleMap.addMarker(markerOptions);
+
+    /*            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     //when map is loaded
                     @Override
                     public void onMapClick(@NonNull LatLng latLng) {
@@ -54,6 +72,7 @@ public class MapFragment extends Fragment {
                         googleMap.addMarker(markerOptions);
                     }
                 });
+     */
             }
         });
 
