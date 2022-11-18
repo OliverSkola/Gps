50b72f84522e49780d4bd939915b0220fb1eefca
aanad <andy.alavinasab@gmail.com>
2022-11-18 23:21:04 +0100

    Finalized formulas aside from elevation

--- app/src/main/java/com/example/gps/CaloriesBurned.java
+++ app/src/main/java/com/example/gps/CaloriesBurned.java
@@ -6,52 +6,25 @@ public class CaloriesBurned {
     double weight;
     double duration;
     double avgVelocity;
-    HashMap<Double, Double> METValues;
+    String activity;
 
     public CaloriesBurned(double weight, double duration, double avgVelocity, String activity) {
         this.weight = weight;
         this.duration = duration;
         this.avgVelocity = avgVelocity;
-        METValues = new HashMap<>();
-        met(activity);
+        this.activity = activity;
     }
 
-    private void met(String activity) {
-        switch (activity) {
-            case "cycling":
-                METValues.put(17.5, 6.8);
-                METValues.put(22.0, 8.0);
-                METValues.put(27.5, 12.0);
-                METValues.put(32.0, 15.0);
-                break;
-            case "running":
-                METValues.put(6.4, 6.0);
-                METValues.put(8.0, 8.3);
-                METValues.put(9.6, 9.8);
-                METValues.put(11.2, 11.0);
-                METValues.put(12.9, 11.8);
-                METValues.put(14.8, 12.8);
-                METValues.put(16.1, 14.5);
-                METValues.put(17.7, 16.0);
-                METValues.put(19.3, 19.0);
-                METValues.put(20.9, 19.8);
-                METValues.put(22.5, 23.5);
-                break;
-            case "walking":
-                METValues.put(4.0, 3.0);
-                METValues.put(4.5, 3.5);
-                METValues.put(5.6, 4.3);
-                METValues.put(6.4, 5.0);
-                METValues.put(7.2, 7.0);
-                METValues.put(8.0, 9.8);
-                break;
-            default: break;
+    private double metEquation(){
+        switch(activity){
+            case "walking": return Math.pow(1.01, 40 * (avgVelocity - 2.6)) + 1;
+            case "running": return Math.pow(1.1, 1.05 * avgVelocity) + 0.6 * avgVelocity + 0.05;
+            case "cycling": return Math.pow(1.5, 0.205 * avgVelocity) + 1.5;
+            default: return 1.0;
         }
-
     }
 
     private double caloriesBurned() {
-        return duration * (METValues.get(avgVelocity) * 3.5 * weight) / 200;
+        return (duration * (metEquation() * 3.5 * weight)) / 200;
     }
-
 }
